7f58925676c57a484d7753696c94d4f7a3393c0f
Ivan Ivanov <ivan@iivan.ru>
2013-01-11 16:58:32 +0000

    Add posttrack.py

---
+++ posttrack.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+from prettytable import PrettyTable
+from suds.client import Client
+import argparse
+import re
+import sys
+
+def barcode_type(s):
+	s = s.upper()
+	if not re.match('^[A-Z]{2}\d{9}[A-Z]{2}$', s):
+		raise argparse.ArgumentTypeError('wrong barcode')
+	return s
+
+parser = argparse.ArgumentParser(description='Show tracking info from Russian Post service.')
+parser.add_argument('barcode', type=barcode_type, help='item barcode in international format')
+args = parser.parse_args()
+
+try:
+	client = Client('http://voh.russianpost.ru:8080/niips-operationhistory-web/OperationHistory?wsdl')
+	history = client.service.GetOperationHistory(Barcode=args.barcode, MessageType=0)
+except Exception as e:
+	sys.exit(e)
+
+table = PrettyTable(['Date', 'Operation', 'Address', 'Weight'])
+for row in history:
+	date = operation = address = weight = ''
+
+	try:
+		date = row.OperationParameters.OperDate
+	except:
+		pass
+
+	try:
+		operation = row.OperationParameters.OperType.Name
+		operation += ' (' + row.OperationParameters.OperAttr.Name + ')'
+	except:
+		pass
+
+	try:
+		address = row.AddressParameters.OperationAddress.Description
+		address = row.AddressParameters.OperationAddress.Index + ' ' + address
+	except:
+		pass
+
+	try:
+		weight = row.ItemParameters.Mass
+		weight /= 1000.0
+	except:
+		pass
+
+	table.add_row([
+		date,
+		operation,
+		address,
+		weight,
+	])
+
+table.set_field_align('Operation', 'l')
+table.set_field_align('Address', 'l')
+table.printt()
